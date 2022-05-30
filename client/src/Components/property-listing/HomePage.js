9d3522990f0aca231951bb492b7386e670b0cdc0
khalid915146 <khalidanwar915146@gmail.com>
2022-05-30 20:12:46 +0530

    HomePage implemented

---
+++ client/src/Components/property-listing/HomePage.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import NavBar from './Navbar';
+import SideBar from './LeftSideBar';
+import Listing from './PropertyListing';
+
+
+function HomePage() {
+  return (<>
+      <NavBar/>
+      <SideBar/>
+      <Listing/>
+
+    </>)}
+
+export default HomePage;
