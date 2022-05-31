5c9ce5c102b8c5c93a95af26b6efc0526e861059
khalid915146 <khalidanwar915146@gmail.com>
2022-05-31 19:52:57 +0530

    signin and sign up page updation

--- client/src/Components/Authentication/SignIn.js
+++ client/src/Components/Authentication/SignIn.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
-const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+const SignIn=()=>{
+  return(<>
+  <div className="form-container">
+        <div className="parent-container">
+        <form className="login-form"><br />
+          <h1 className="logo">Logo</h1>
+          <p>Enter your credentials to access your account</p><br /><br />
+          <div className="mb-3">
+            {/* <label htmlFor='exampleInputEmail1' className="form-label"> </label> */}
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="User ID"
+            />
+          </div><br />
+          <div className="mb-3">
+            {/* <label htmlFor='exampleInputPassword1' className="form-label"></label> */}
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div><br /><br />
+          <button type="submit" className="btn-primary">
+            Sign In
+          </button>
+          <br /><br />
+          <button type="submit" className="btn-light">
+            Sign Up
+          </button>
+        </form>
+        <br /><br />
+        <div className="bottom-container">
+          <p>
+            Don't have an account? <a href="/register">Sign up</a>
+          </p>
+        </div>
+      </div>
+      </div>
+  </>)
 }
-
-export default SignIn
+export default SignIn;
--- client/src/Components/Authentication/SignUp.js
+++ client/src/Components/Authentication/SignUp.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+const SignUp=()=>{
+  return(<>
+  <div className="form-container">
+        <div className="parent-container">
+        <form className="login-form"><br />
+          <h1 className="logo">Logo</h1>
+          <p>Enter your credentials to access your account</p><br /><br />
+          <div className="mb-3">
+            {/* <label htmlFor='exampleInputEmail1' className="form-label"> </label> */}
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="User ID"
+            />
+          </div><br />
+          <div className="mb-3">
+            {/* <label htmlFor='exampleInputPassword1' className="form-label"></label> */}
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div><br /><br />
+          <button type="submit" className="btn-primary">
+            Sign Up
+          </button>
+          <br /><br />
+          {/* <button type="submit" className="btn-light">
+            Sign Up
+          </button> */}
+        </form>
+        <br /><br />
+        <div className="bottom-container">
+          <p>
+            <a href="/register">Sign In</a>
+          </p>
+        </div>
+      </div>
+      </div>
+  </>)
 }
-
-export default SignUp
+export default SignUp;
